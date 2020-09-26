8f3dbd408cfd7d17429f44e088d59d5d8bc7bb9a
AdityaAWP <aditwprx@gmail.com>
2020-09-26 11:39:09 +0700

    Update

--- app/src/main/java/com/example/myapplication/DataAdapter.java
+++ app/src/main/java/com/example/myapplication/DataAdapter.java
@@ -16,7 +16,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.target.Target;
 
 import java.util.ArrayList;
 
--- app/src/main/java/com/example/myapplication/ListDataFavourite.java
+++ app/src/main/java/com/example/myapplication/ListDataFavourite.java
@@ -1,21 +1,27 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
 import java.util.ArrayList;
+import java.util.List;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class ListDataFavourite extends AppCompatActivity {
-
+    Realm realm;
     private RecyclerView recyclerView;
-    private DataAdapterFavourite adapter;
-    private ArrayList<MovieModel> DataArrayList; //kit add kan ke adapter
+    private DataAdapter adapter;
+    private ArrayList<Model> DataArrayList; //kit add kan ke adapter
     private ImageView tambah_data;
 
     @Override
@@ -23,7 +29,32 @@ public class ListDataFavourite extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_data);
         recyclerView = (RecyclerView) findViewById(R.id.rvdata);
+        DataArrayList = new ArrayList<>();
+        adapter = new DataAdapter(DataArrayList, new DataAdapter.Callback() {
+            @Override
+            public void onClick(int position) {
+                Model movie = DataArrayList.get(position);
+                Intent intent = new Intent(getApplicationContext(), DetailMovie.class);
+                intent.putExtra("judul",movie.original_title);
+                intent.putExtra("date",movie.release_date);
+                intent.putExtra("deskripsi",movie.overview);
+                intent.putExtra("path",movie.poster_path);
+                startActivity(intent);
+            }
 
+            @Override
+            public void test() {
+
+            }
+        });
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ListDataFavourite.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+    }
+    public List<MovieModel> getAllMahasiswa() {
+        RealmResults<MovieModel> results = realm.where(MovieModel.class).findAll();
+        return results;
     }
 
 
